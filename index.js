903dbe9c0e8f3b840b2cd6350db376f556c69878
guptabhoomika <43852276+guptabhoomika@users.noreply.github.com>
2020-09-21 11:25:35 +0530

    cors fix 3

--- index.js
+++ index.js
@@ -27,12 +27,6 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", *); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 
 
 
